54bcdfedd9368553bdfb876fe0e8fd27d7afcf39
Roman Kuzmich <rvku2mich@yandex.ru>
2026-02-24 07:35:37 +0300

    initial commit

---
+++ src/test/java/ru/kuzmich/springdatajdbc/SpringDataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuzmich.springdatajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
